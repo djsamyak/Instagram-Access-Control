cb720ae360aeb8cd0a987891b8d8e5996df25b7f
djsamyak <djsamyak@gmail.com>
2020-08-29 22:03:18 +0400

    Inital .exe

---
+++ backend.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import random
+import string
+import smtplib
+
+def GET_old_passcode():
+    try:
+        data = pd.read_csv('old_code.txt',header=None)
+        return data[0][0]
+    except:
+        return data["password"][0]
+
+def POST_new_passcode(password):
+    f = open("old_code.txt", "w")
+    f.seek(0) 
+    f.write(password)
+    f.close()
+
+def APPEND_password(password):
+    f = open("list.txt", "a")
+    f.write(password)
+    f.write("\n")
+    f.close()
+
+
+def generate_passcode(length):
+    # Random string with the combination of lower and upper case
+    letters_and_digits = string.ascii_letters + string.digits
+    result_str = ''.join((random.choice(letters_and_digits) for i in range(length)))
+    POST_new_passcode(result_str + "\n")
+    APPEND_password(result_str + "\n")
+    return result_str
+
+def send_Mail(data):
+    conn = smtplib.SMTP("smtp.gmail.com",587)
+
+    userID = data["BOT_ID"][0]
+    password = data["BOT_PASSWORD"][0]
+
+    conn.ehlo()
+    conn.starttls()
+    conn.login(userID,password)
+
+    message = f'''Subject: Updated Password for {data["username"][0]} \r\n
+Dear User,
+The updated password for {data["username"][0]} is {GET_old_passcode()}.'''
+    print(message)
+
+    conn.sendmail("NODATA",data["NOTIF_ID"][0],message)
+    conn.quit()
---
+++ bot.py
@@ -0,0 +1,65 @@
+import time
+from selenium import webdriver
+import bs4
+import requests
+import getpass
+import sys
+from backend import *
+import pandas as pd
+
+data = pd.read_csv("requirements.csv")
+
+try:
+    browser=webdriver.Chrome(r"C:\Users\djsam\Desktop\Files\Selenium_Drivers\chromedriver")
+except:
+    browser=webdriver.Firefox()
+
+browser.maximize_window()
+browser.implicitly_wait(20)
+browser.get("https://www.instagram.com/")
+time.sleep(1)
+
+userID = data["username"][0]
+try:
+    userPasscode = GET_old_passcode()
+except:
+    userPasscode = data["password"][0]
+    f = open("old_code.txt", "w")
+    f.seek(0) 
+    f.write(userPasscode)
+    f.close()
+
+login_username_Field=browser.find_element_by_css_selector("#loginForm > div > div:nth-child(1) > div > label > input")
+login_username_Field.send_keys(userID)
+
+login_passcode_Field=browser.find_element_by_css_selector("#loginForm > div > div:nth-child(2) > div > label > input")
+login_passcode_Field.send_keys(userPasscode)
+
+login_passcode_Field.submit()
+
+time.sleep(10)
+saveLogin = browser.find_element_by_css_selector("#react-root > section > main > div > div > div > div > button")
+if saveLogin:
+    saveLogin.click()
+notifNOTNOW=browser.find_element_by_css_selector("body > div.RnEpo.Yx5HN > div > div > div > div.mt3GC > button.aOOlW.HoLwm")
+notifNOTNOW.click()
+
+browser.get("https://www.instagram.com/accounts/password/change/")
+
+oldPasscodeEntry = browser.find_element_by_css_selector("#cppOldPassword")
+oldPasscodeEntry.send_keys(GET_old_passcode())
+
+APPEND_password(GET_old_passcode())
+
+newPasscodeEntry = browser.find_element_by_css_selector("#cppNewPassword")
+newPasscodeEntry.send_keys(generate_passcode(10))
+
+confirmPasscodeEntry = browser.find_element_by_css_selector("#cppConfirmPassword")
+confirmPasscodeEntry.send_keys(GET_old_passcode())
+
+# changePasscode = browser.find_element_by_css_selector("#react-root > section > main > div > article > form > div:nth-child(4) > div > div > button")
+
+send_Mail(data)
+browser.close()
+
+
